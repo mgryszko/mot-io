ca0d3fbb194b20541f5cffdbbb4e0298ee519a4c
mgryszko <marcin.gryszko@lastminute.com>
2020-12-05 09:37:25 +0100

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,3 @@
+scalaVersion := "2.13.4"
+
+libraryDependencies += "org.scalatest" % "scalatest_2.13" % "3.2.3" % "test"
---
+++ src/test/scala/Test.scala
@@ -0,0 +1,7 @@
+import org.scalatest.funsuite.AnyFunSuite
+
+class Test extends AnyFunSuite {
+  test("test is run") {
+    assert(true === false)
+  }
+}
