6ac06fd2e74bbdd30e9140b216871c841f8ad2a7
mgryszko <marcin.gryszko@lastminute.com>
2020-12-10 06:22:54 +0100

    Naive IO crashing with stack overflow

---
+++ src/test/scala/IO.scala
@@ -0,0 +1,38 @@
+import org.scalatest.funsuite.AnyFunSuite
+
+sealed trait IO[A] {
+  self =>
+  def run: A
+
+  def map[B](f: A => B): IO[B] =
+    new IO[B] {
+      def run: B = f(self.run)
+    }
+
+  def flatMap[B](f: A => IO[B]): IO[B] =
+    new IO[B] {
+      def run: B = f(self.run).run
+    }
+}
+
+object IO {
+  def unit[A](a: => A): IO[A] = new IO[A] {
+    def run: A = a
+  }
+
+  def flatMap[A, B](fa: IO[A])(f: A => IO[B]): IO[B] = fa flatMap f
+
+  def apply[A](a: => A): IO[A] = unit(a)
+
+  def forever[A, B](a: IO[A]): IO[B] = {
+    lazy val t: IO[B] = forever(a)
+    a.flatMap(_ => t)
+  }
+}
+
+class IOTest extends AnyFunSuite {
+  test("stack overflow") {
+    IO.forever(IO { println("Waiting...") }).run
+  }
+}
+
--- src/test/scala/Test.scala
+++
@@ -1,8 +0,0 @@
-import cats.syntax.all._
-import org.scalatest.funsuite.AnyFunSuite
-
-class Test extends AnyFunSuite {
-  test("test is run") {
-    assert(true.show == false.show)
-  }
-}
