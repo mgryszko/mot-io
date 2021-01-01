c595b6a625b7ad29cd2815827fbdabf8a30d70cf
mgryszko <marcin.gryszko@lastminute.com>
2021-01-01 17:03:15 +0100

    Error handling with stack

--- src/test/scala/io/IOStack.scala
+++ src/test/scala/io/IOStack.scala
@@ -3,33 +3,38 @@ package io
 import org.scalatest.funsuite.AnyFunSuite
 
 import scala.annotation.tailrec
+import scala.util.{Failure, Success, Try}
 
 sealed trait Step
-case class Done[A](x: A) extends Step
+case class Done[A](x: Try[A]) extends Step
 case class Eval[A](m: IO[A]) extends Step
 case class Continue[A, B](f: A => IO[B]) extends Step
 
 object IOStackRun {
   private def eval[A, B](io: IO[A]): List[Step] = {
     io match {
-      case Return(x) => Done(x) :: Nil
-      case Suspend(x) => Done(x()) :: Nil
+      case Return(x) => Done(Success(x)) :: Nil
+      case Suspend(x) => Done(Try(x())) :: Nil
       case FlatMap(m: IO[A], f: (A => IO[B])) => Eval(m) :: Continue(f) :: Nil
     }
   }
 
   @tailrec
-  private def process[A, B](eventLoop: List[Step]): A = {
+  private def process[A, B](eventLoop: List[Step]): Try[A] = {
     eventLoop match {
-      case Done(x: A) :: Nil => x
-      case Done(x: A) :: Continue(f: (A => IO[B])) :: rest => process(Eval(f(x)) :: rest)
+      case Done(res: Try[A]) :: Nil => res
+      case Done(res: Try[A]) :: Continue(f: (A => IO[B])) :: rest => res match {
+        case Success(x) => Try(f(x)) match {
+          case Success(fx) => process(Eval(fx) :: rest)
+          case Failure(e) => Failure(e)
+        }
+        case Failure(e) => Failure(e)
+      }
       case Eval(m) :: rest => process(eval(m) ++ rest)
     }
   }
 
-  def runSync[A](io: IO[A]): A = {
-    process(eval(io)).asInstanceOf[A]
-  }
+  def runSync[A](io: IO[A]): Try[A] = process(eval(io))
 }
 
 class IOStackTest extends AnyFunSuite {
@@ -46,7 +51,26 @@ class IOStackTest extends AnyFunSuite {
       .flatMap(x =>
         IO(x + 3)
       )
-    assert(runSync(io) == 7)
+    assert(runSync(io) == Success(7))
+  }
+
+  test("error") {
+    val exception = new IllegalArgumentException("bang!")
+
+    val returnFlatmapIo = IO(1).flatMap(_ => throw exception)
+    assert(runSync(returnFlatmapIo) == Failure(exception))
+
+    val suspendFlatmapIo = IO.effect { () => 1 }.flatMap(_ => throw exception)
+    assert(runSync(suspendFlatmapIo) == Failure(exception))
+
+    val suspendFlatmapIo2 = IO.effect { () => throw exception; 1 }.flatMap(x => IO(x + 2))
+    assert(runSync(suspendFlatmapIo2) == Failure(exception))
+
+    val multiFlatmapIo = IO(1).flatMap { x => throw exception; IO(x + 1) }.flatMap(x => IO(x + 2))
+    assert(runSync(multiFlatmapIo) == Failure(exception))
+
+    val multiFlatmapIo2 = IO(1).flatMap(x => IO(x + 1)).flatMap { x => throw exception; IO(x + 2) }
+    assert(runSync(multiFlatmapIo2) == Failure(exception))
   }
 
   test("stack overflow") {
